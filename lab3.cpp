001490aa0432de73f619d33fc871849bcea01c85
lycurtis <137116222+lycurtis@users.noreply.github.com>
2024-04-24 19:49:24 -0700

    Create lab3.cpp

---
+++ lab3.cpp
@@ -0,0 +1,96 @@
+#include "fsl_device_registers.h"
+#include <stdint.h>
+
+unsigned char decoder[10] = {0x7E, 0x30, 0x6D, 0x79, 0x33, 0x5B, 0x5F, 0x70, 0x7F, 0x7B}; //display values 0-9 on 7 segment
+unsigned int count = 0;
+unsigned int value = 0;
+
+unsigned short ADC_Read16B(void){
+	ADC0_SC1A = 0x00; //Write to SC1A to start conversion 
+	while(ADC0_SC2 & ADC_SC2_ADACT_MASK); //conversion in process
+	while(!(ADC0_SC1A & ADC_SC1_COCO_MASK)); //Until conversion is complete
+	return ADC0_RA; //ADC conversion result for ADC0
+}
+
+void PORTA_IRQHandler(void){
+	//Toggle ones place decimal point
+	NVIC_ClearPendingIRQ(PORTA_IRQn); //clear pending interrupts
+	GPIOD_PTOR |= 0xFF; //Toggle
+	unsigned int input = GPIOB_PDIR;//Read port B
+	unsigned int MODE_SW = input & 0x04; //PB2
+	unsigned int CNT_DIR = input & 0x08; //PB3
+
+	if(!MODE_SW){ //ADC mode
+		//read from ADC and convert to decimal value
+		value = ADC_Read16B();
+	}
+	else if(MODE_SW){ //Count Mode
+		if(!CNT_DIR){
+			//count up to 99 and roll over to 0
+			if(count < 99){
+				count++;
+			}
+			else{
+				count = 0;
+			}
+		}
+		else{
+			//count down to 0 and roll over to 99
+			if(count > 0){
+				count--;
+			}
+			else{
+				count = 99;
+			}
+		}
+	}
+
+	//Display value based on MODE_SW
+	if(MODE_SW){ //count mode
+		value = count;
+	}
+	else{ //ADC mode 0-255
+		 //scale down 0-255 to 0-3.3V
+	}
+	PORTA_ISFR = (1 << 1); //Clear ISFR for PORTA, Pin 1
+	GPIOD_PCOR = 0xFF; //clears output on PortD[0:7]
+	GPIOC_PCOR = 0xFF; //clears output on PortC[0:7]
+	GPIOD_PSOR = (unsigned int)decoder[value/10]; //sets output to converted value PortD
+	GPIOC_PSOR = ((((unsigned int)decoder[value%10]) & 0x40) << 1) | ((unsigned int)decoder[value%10] & 0x3F); //sets output to converted value PortC
+}
+
+int main(void)
+{
+	//clock gating
+	SIM_SCGC5 |= SIM_SCGC5_PORTA_MASK; /* Enable Port A Clock Gate Control */
+	SIM_SCGC5 |= SIM_SCGC5_PORTB_MASK; /*Enable Port B Clock Gate Control*/
+	SIM_SCGC5 |= SIM_SCGC5_PORTC_MASK; /*Enable Port C Clock Gate Control*/
+	SIM_SCGC5 |= SIM_SCGC5_PORTD_MASK; /*Enable Port D Clock Gate Control*/
+	SIM_SCGC6 |= SIM_SCGC6_ADC0_MASK; /*Enable Port ADC Clock Gate Control*/
+	ADC0_CFG1 = 0x0C;
+	
+	//configuration & initialization
+	PORTD_GPCLR = 0xFF0100; //Configure pins 0-7 on PORT D to be GPIO
+	PORTC_GPCLR = 0x1BF0100; //Configure pins 0-5, 7-8 on PORT C to be GPIO (skip PTC 6 because that is reserved by the K64F for interrupt)
+	PORTB_GPCLR = 0x40C0100; //configures pins 2,3, and 10 on port B to be GPIO
+	PORTA_PCR1 = 0xA0100; /* Configure Port A pin 1 for GPIO and interrupt on falling edge */
+	
+	//Direction registers
+	GPIOD_PDDR = 0xFF; //Configures pins 0-7 of Port D as output
+	GPIOC_PDDR = 0xFF; //configure Port C pins 0-5 7-8 for output
+	GPIOA_PDDR = 0x00; //Configure PortA1 to be input
+	GPIOB_PDDR = 0x400; //Configure Port B pins 2-3 to for Input reading from the switch and pin 10 for output
+	
+	//enable interrupt service routine
+	PORTA_ISFR = (1 << 1); /* Clear ISFR for Port A, Pin 1*/
+	NVIC_EnableIRQ(PORTA_IRQn); /* Enable interrupts from Port A*/
+	
+	//Artificial Clock generation
+	for(;;){
+		GPIOB_PTOR |= (1<<10);
+		for(int i = 0; i < 1000; i++);//delay for loop
+	}
+	
+	
+	return 0;
+}
