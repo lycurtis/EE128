d3f5049ad0788e6965edd68b7c8bd27d7dd7a2d4
lycurtis <137116222+lycurtis@users.noreply.github.com>
2024-05-01 18:49:52 -0700

    Lab4.cpp

--- Lab4.cpp
+++ Lab4.cpp
@@ -4,27 +4,30 @@
 unsigned char decoder[10] = {0x7E, 0x30, 0x6D, 0x79, 0x33, 0x5B, 0x5F, 0x70, 0x7F, 0x7B}; //display values 0-9 on 7 segment
 unsigned int nr_overflows = 0;
 unsigned int value = 0;
+unsigned int t1, t2, t3, pulse_width, duty_cycle, period;
 
 void FTM3_IRQHANDLER(void) {
     nr_overflows++;
     uint32_t SC_VAL = FTM3_SC;
     FTM3_SC &= 0x7F; //clear TOF
+    //FTM3_SC &= ~(0x80); //clear TOF
 
-    //display value onto 7 segment 
-    GPIOD_PCOR = 0x7F; //clears output on PortD[0:6] DO NOT CLEAR PIN 7 because pin 7 is reserved for toggle
-    GPIOC_PCOR = 0xBF; //clears output on PortC
-    GPIOD_PSOR = (unsigned int)decoder[(int)value/10]; //sets output to converted value PortD
-    GPIOC_PSOR = ((((unsigned int)decoder[(int)value%10]) & 0x40) << 1) | ((unsigned int)decoder[(int)value%10] & 0x3F);
 }
 
 void main(void) {
-    SIM_SCGC5 |= SIM_SCGC5_PORTC_MASK; //Port C clock enable
+	SIM_SCGC5 |= SIM_SCGC5_PORTC_MASK; //Port C clock enable
     SIM_SCGC5 |= SIM_SCGC5_PORTD_MASK; //Port D clock enable
 
     SIM_SCGC3 |= SIM_SCGC3_FTM3_MASK; //FTM3 clock enable
 
     PORTC_PCR10 = 0x300; //Port C Pin 10 as FTM3_CH6 (ALT3)
     PORTD_PCR10 = 0x300; //Port D Pin 10 as FTM3_CH6 (ALT3)
+    PORTD_GPCLR = 0xFF0100; //Configure pins 0-7 on PORT D to be GPIO
+    PORTC_GPCLR = 0x1BF0100; //Configure pins 0-5, 7-8 on PORT C to be GPIO (skip PTC 6 because that is reserved by the K64F for interrupt)
+
+    //Direction registers
+    GPIOD_PDDR = 0xFF; //Configures pins 0-7 of Port D as output
+    GPIOC_PDDR = 0xFF; //configure Port C pins 0-7 for output
 
     FTM3_MODE = 0x5; //Enable FTM3
     FTM3_MOD = 0xFFFF;
@@ -33,7 +36,6 @@ void main(void) {
     NVIC_EnableIRQ(FTM3_IRQn); //Enable FTM3 interrupts
     FTM3_SC |= 0x40; //Enable TOF
 
-    unsigned int t1, t2, t3, pulse_width, duty_cycle, period;
     while (1) {
         FTM3_CNT = 0; nr_overflows = 0; // initialize counters
         FTM3_C6SC = 0x4; // rising edge
@@ -63,6 +65,12 @@ void main(void) {
 
         duty_cycle = (pulse_width * 100)/period;
         value = duty_cycle;
-        //for (int i = 0; i < 100000; i++); // breakpoint here for debugging
+
+        //display value onto 7 segment
+        GPIOD_PCOR = 0x7F; //clears output on PortD[0:6] DO NOT CLEAR PIN 7 because pin 7 is reserved for toggle
+        GPIOC_PCOR = 0xBF; //clears output on PortC
+        GPIOD_PSOR = (unsigned int)decoder[(int)value/10]; //sets output to converted value PortD
+        GPIOC_PSOR = ((((unsigned int)decoder[(int)value%10]) & 0x40) << 1) | ((unsigned int)decoder[(int)value%10] & 0x3F);
+
     }
 }
